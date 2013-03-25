aaa06a7bb7a0dffbc2bee2030663f0baec760bb4
Joshua Cohen <joshua+github@heyjoshua.com>
2013-03-24 21:47:13 -0500

    Move IBOutlets/IBActions to private category in the implementation. No reason for them to be public

--- WordPress/Classes/QuickPostViewController.h
+++ WordPress/Classes/QuickPostViewController.h
@@ -12,17 +12,6 @@
 
 @interface QuickPostViewController : UIViewController<UITableViewDataSource, UITableViewDelegate, UITextFieldDelegate, UITextViewDelegate>
 
-@property (nonatomic, strong) IBOutlet UIButton *choosePhotoButton;
-@property (nonatomic, strong) IBOutlet UIView *containerView;
-@property (nonatomic, strong) IBOutlet UIButton *detailsButton;
-@property (nonatomic, strong) IBOutlet UITableView *detailsTableView;
-@property (nonatomic, strong) IBOutlet UITextView *bodyTextView;
-@property (nonatomic, strong) IBOutlet UIView *photoSelectionMethodView;
-@property (nonatomic, strong) IBOutlet UILabel *placeholderLabel;
 @property (nonatomic, weak) SidebarViewController *sidebarViewController;
-@property (nonatomic, strong) IBOutlet UITextField *titleTextField;
-
-- (IBAction)choosePhotoButtonTapped:(id)sender;
-- (IBAction)detailsButtonTapped:(id)sender;
 
 @end
